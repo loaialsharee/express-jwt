b66dbd3fe83a526f2703c068af76da939c9ae21c
Loai Al-Sharee <loaialsharee1@gmail.com>
2023-11-18 23:09:07 +0800

    Set up project baseline

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+mongoose.connect(process.env.MONGODB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => res.render('home'));
+app.get('/receipes', (req, res) => res.render('receipes'));
