262bbfe01d2b4c301636791e4d8f779e4ced80a8
Loai Al-Sharee <loaialsharee1@gmail.com>
2023-11-20 22:08:29 +0800

    Add bcrypt to hash user password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const {isEmail} = require('validator')
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -15,6 +16,14 @@ const userSchema = new mongoose.Schema({
         minlength: [6, 'Minimum password length is 6 characters'],
     }
 });
+
+// This function will run before the doc is saved to db
+userSchema.pre('save', async function(next){
+    // Attach a 'salt' - an additional string added to the password for an extra layer of protection before hashing
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
 
 const User = mongoose.model('user', userSchema)
 
