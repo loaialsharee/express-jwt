d40156ab99658f34f00721615250684f169fe7ae
Loai Al-Sharee <loaialsharee1@gmail.com>
2024-01-07 04:11:45 +0800

    Set JWT token once user signs up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,13 @@
 const User = require('../models/User');
 const { handleErrors } = require('../utils/handleErrors');
+const jwt = require('jsonwebtoken');
+
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({id}, process.env.secret, {
+    expiresIn: maxAge
+  })
+}
 
 module.exports.getSignup = (req, res) => {
   res.render("signup");
@@ -14,7 +22,9 @@ module.exports.postSignup = async (req, res) => {
 
   try{
     const user = await User.create({email, password});
-    res.status(201).json(user);
+    const token = createToken(user._id)
+    res.cookie('jwt', token, {httpOnly:true, maxAge: maxAge*1000})
+    res.status(201).json({user: user._id});
   }catch (err) {
     const errors = handleErrors(err)
     res.status(400).json({errors});
